89e3b5ca120899610cb87aa3f55504c8282cd562
amiralitaheri <amiralitaheri64@gmail.com>
2020-08-18 02:05:12 +0430

    notification dot

--- lib/components/notification_icon.dart
+++ lib/components/notification_icon.dart
@@ -3,5 +3,27 @@ import 'package:vegetables_app_training_ui/utils/colors.dart';
 
 class NotificationIcon extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Icon(Icons.notifications_none, color: black,);
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8),
+        child: Stack(children: [
+          Icon(
+            Icons.notifications_none,
+            color: black,
+          ),
+          Positioned(
+            top: 2,
+            right: 5,
+            child: SizedBox(
+              height: 8,
+              width: 8,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                  color: red,
+                ),
+              ),
+            ),
+          )
+        ]),
+      );
 }
